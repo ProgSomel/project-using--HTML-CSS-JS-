35b4638d594b531f35b7dab7898a2df326a8c3bb
ProgSomel <somelahmed55@gmail.com>
2025-03-27 07:33:27 +0600

    gsap updated

--- LandingPage04/script.js
+++ LandingPage04/script.js
@@ -1,19 +1,30 @@
+// Animate .imagecontainer width to 100% (forward, then reset)
 gsap.to(".imagecontainer", {
     width: "100%",
-    ease: Expo.easeInOut,
+    ease: "expo.inOut",
     duration: 2,
     stagger: 2,
-    
-})
+    repeat: -1,  // Infinite loop (no yoyo)
+    repeatDelay: 0 // Optional delay before repeat
+});
 
+// Animate text h1 down (top: 0)
 gsap.to(".text h1", {
-    ease: Expo.easeInOut,
+    top: 0,
+    ease: "expo.inOut",
+    duration: 2,
     stagger: 2,
-    top: 0
-})
+    repeat: -1,  // Infinite loop
+    repeatDelay: 0
+});
+
+// Animate text h1 up (top: -100%) after delay
 gsap.to(".text h1", {
-    delay: 2,
-    ease: Expo.easeInOut,
+    top: "-100%",
+    delay: 2,  // Starts after 2 seconds
+    ease: "expo.inOut",
+    duration: 2,
     stagger: 2,
-    top: "-100%"
-})
+    repeat: -1,  // Infinite loop
+    repeatDelay: 0
+});
